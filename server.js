4eaa7153d0a2e0a2e84bbb342ee88718ebbd7a08
Tanner Goshert <tgosh206@hotmail.com>
2018-10-24 13:32:49 -0700

    edited code, added package-lock

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-app.delete('/shopping-list/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.id}\``);
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe for \`${req.params.id}\``);
   res.status(204).end();
 });
 
